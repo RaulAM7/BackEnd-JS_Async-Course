5fe445bfc21dd096e34dfcbebdd042a5d866cf01
raulAM7 <raulartilesm@gmail.com>
2025-01-02 22:49:02 +0000

    update

--- Class2/js-chained-promises.js
+++ Class2/js-chained-promises.js
@@ -1,18 +1,18 @@
 // Encadenamiento de promesas, util para cuando necesitas que varias operaciones async dependan entre sí
 
 
-const paso1 = Promise.resolve('Paso 1 completado')
-const paso2 = Promise.resolve('Paso 2 completado')
-const paso3 = Promise.resolve('Paso 3 completado')
+const paso1 = () => Promise.resolve('Paso 1 completado')
+const paso2 = () => Promise.resolve('Paso 2 completado')
+const paso3 = () => Promise.resolve('Paso 3 completado')
 
-paso1
+paso1()
     .then((resultado1) => {
         console.log(resultado1)
-        return paso2
+        return paso2()
     })
     .then((resultado2) => {
         console.log(resultado2)
-        return paso3
+        return paso3()
     })
     .then((resultado3) => {
         console.log(resultado3)
