996825a54b1d306047bd676e59989a80a90f940c
raulAM7 <raulartilesm@gmail.com>
2025-01-03 19:40:24 +0000

    update

--- Class2/js-promises.syntax.js
+++ Class2/js-promises.syntax.js
@@ -31,11 +31,29 @@ function consumirMiPromesa() {
 
 const promesaResuelta = Promise.resolve('Promesa resuelta por defecto')
 
-function consumirPromesaResuelta(){
+function consumirPromesaResuelta() {
     promesaResuelta
         .then((a) => {console.log('Exito: ', a)})
         .catch((b) => {console.log('Error: ', b)})
 }
 // consumirPromesaResuelta()
 
-        
+
+// 4.- ENCADENAMIENTO de promesas
+const paso1 = () => Promise.resolve('Promesa resuleta por defecto 1') 
+const paso2 = () => Promise.resolve('Promesa resuleta por defecto 2') 
+
+function consumirPromesasResuletasChained() {    
+    paso1()
+        .then((resultado) => {
+            console.log('Este es el resultado de la promesa 1: ', resultado)
+            paso2(resultado)
+        })
+        .then((resultado) => {
+            console.log('Este es el resultado de la promesa 2: ', resultado)
+        })
+        .catch((error) => {console.log('Error consumiendo promesas chained', error)})
+}
+// consumirPromesasResuletasChained()
+
+// 5.- MANEJO DE ERRORES 
